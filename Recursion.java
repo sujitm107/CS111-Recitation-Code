cc792c7225789a803befa1b1ac90bd9174916031
Sujit <sujit.mollet@rutgers.edu>
2020-03-23 13:08:44 -0400

    added Recursion.java

---
+++ Recursion.java
@@ -0,0 +1,40 @@
+public class Recursion{
+
+    public static int factorial(int n){
+        
+        return 0;
+    }
+
+    public static int fib(int n){
+     
+        return 0;
+    }
+
+    public static int pyramid(){
+        public 
+    }
+
+    public static double harmonic(int x){
+
+        return 0;
+    }
+
+    public static void printRow(int nStars){
+        if(nStars == 0){
+            System.out.println(); //Force a new line
+            return; //no more recursion
+        }
+        System.out.print("*");
+        printRow(nStars - 1);
+    }
+
+    public static void main(String[] args){
+
+        System.out.println(factorial(7));
+
+        System.out.println();
+
+        System.out.println();
+
+    }
+}
