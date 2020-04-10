5a00f82bcda5d01eecf0e5bdfbcb2f4f843bce4b
Sujit <sujit.mollet@rutgers.edu>
2020-04-09 20:16:42 -0400

    done with Section 53

--- BankAccount/BankAccount.java
+++ BankAccount/BankAccount.java
@@ -15,11 +15,65 @@ package BankAccount;
  * 4. In another file, create a simple test drive class that creates an instance of your bank account
  *    class, executes the sequences you came up with in part 3, and display the results
  */
+//import java.utll.*;
 
 public class BankAccount {
     //Fields
+    String name;
+    double checkingBalance;
+    double savingsBalance;
+    int pin;
+    //ArrayList<Double> transactions = new ArrayList<Double>();
 
     //Constructor
+    public BankAccount(String name, double icheckingAmount, double isavingsAmount, int pin){
+        this.name = name;
+        this.checkingBalance = icheckingAmount;
+        this.savingsBalance = isavingsAmount;
+        this.pin = pin;
+    }
 
     //instance methods
+    public double withdrawChecking(double amount){
+        if(amount < checkingBalance){
+            checkingBalance -= amount;
+            //transactions.add(-1*amount);
+        }
+        return amount;
+    }
+
+    public double withdrawSavings(double amount){
+        if(amount < savingsBalance){
+            savingsBalance -= amount;
+            //transactions.add(-1*amount);
+        }
+        return amount;
+    }
+
+    public void depositChecking(double amount){
+        checkingBalance += amount;
+        //transactions.add(amount);
+    }
+
+    public void depositSavings(double amount){
+        checkingBalance += amount;
+        //transactions.add(amount);
+    }
+
+    public double getcheckingBalanceAmount(){
+        return checkingBalance;
+    }
+
+    public double getsavingsBalanceAmount(){
+        return savingsBalance;
+    }
+
+    public void changePin(int newPin){
+        pin = newPin;
+    }
+
+    public int getPin(){
+        return pin;
+    }
+
 }
